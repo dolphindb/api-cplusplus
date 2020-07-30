5c0592fb8e3912dae809f3ba7fe60e0ab37f4e8a
xiaodanzhang <xiaodanzhang@users.noreply.github.com>
2020-07-29 23:35:34 -0400

    Add files via upload

--- include/ConstantMarshall.h
+++ include/ConstantMarshall.h
@@ -10,6 +10,11 @@
 
 #include "DolphinDB.h"
 #include "SysIO.h"
+#ifdef _MSC_VER
+#define EXPORT_DECL _declspec(dllexport)
+#else
+#define EXPORT_DECL 
+#endif
 
 #define MARSHALL_BUFFER_SIZE 4096
 
@@ -25,7 +30,7 @@ typedef SmartPointer<CodeUnmarshall> CodeUnmarshallSP;
 typedef SmartPointer<ConstantMarshallFactory> ConstantMarshallFactorySP;
 typedef SmartPointer<ConstantUnmarshallFactory> ConstantUnmarshallFactorySP;
 
-class ConstantMarshallImp : public ConstantMarshall {
+class EXPORT_DECL ConstantMarshallImp : public ConstantMarshall {
 public:
 	ConstantMarshallImp(const DataOutputStreamSP& out):out_(out), complete_(false){}
 	virtual ~ConstantMarshallImp(){}
@@ -44,7 +49,7 @@ protected:
 	char buf_[MARSHALL_BUFFER_SIZE];
 };
 
-class ConstantUnmarshallImp : public ConstantUnmarshall{
+class EXPORT_DECL ConstantUnmarshallImp : public ConstantUnmarshall{
 public:
 	ConstantUnmarshallImp(const DataInputStreamSP& in):in_(in){}
 	virtual ~ConstantUnmarshallImp(){}
@@ -56,7 +61,7 @@ protected:
 };
 
 
-class ScalarMarshall: public ConstantMarshallImp{
+class EXPORT_DECL ScalarMarshall: public ConstantMarshallImp{
 public:
 	ScalarMarshall(const DataOutputStreamSP& out):ConstantMarshallImp(out), partial_(0){}
 	virtual ~ScalarMarshall(){}
@@ -66,7 +71,7 @@ private:
 	int partial_;
 };
 
-class VectorMarshall: public ConstantMarshallImp{
+class EXPORT_DECL VectorMarshall: public ConstantMarshallImp{
 public:
 	VectorMarshall(const DataOutputStreamSP& out):ConstantMarshallImp(out),nextStart_(0),partial_(0){}
 	virtual ~VectorMarshall(){}
@@ -79,7 +84,7 @@ private:
 	ConstantMarshallSP marshall_;
 };
 
-class MatrixMarshall: public ConstantMarshallImp{
+class EXPORT_DECL MatrixMarshall: public ConstantMarshallImp{
 public:
 	MatrixMarshall(const DataOutputStreamSP& out) : ConstantMarshallImp(out), rowLabelSent_(false),
 		columnLabelSent_(false), inProgress_(false), vectorMarshall_(out){}
@@ -96,7 +101,7 @@ private:
 	VectorMarshall vectorMarshall_;
 };
 
-class TableMarshall: public ConstantMarshallImp{
+class EXPORT_DECL TableMarshall: public ConstantMarshallImp{
 public:
 	TableMarshall(const DataOutputStreamSP& out) : ConstantMarshallImp(out) ,columnNamesSent_(0), nextColumn_(0),
 		columnInProgress_(false), vectorMarshall_(out){}
@@ -113,7 +118,7 @@ private:
 	VectorMarshall vectorMarshall_;
 };
 
-class SetMarshall: public ConstantMarshallImp{
+class EXPORT_DECL SetMarshall: public ConstantMarshallImp{
 public:
 	SetMarshall(const DataOutputStreamSP& out):ConstantMarshallImp(out), vectorMarshall_(out){}
 	virtual ~SetMarshall(){}
@@ -127,7 +132,7 @@ private:
 	VectorMarshall vectorMarshall_;
 };
 
-class DictionaryMarshall: public ConstantMarshallImp{
+class EXPORT_DECL DictionaryMarshall: public ConstantMarshallImp{
 public:
 	DictionaryMarshall(const DataOutputStreamSP& out):ConstantMarshallImp(out), keySent_(false),
 		inProgress_(false), vectorMarshall_(out){}
@@ -143,7 +148,7 @@ private:
 	VectorMarshall vectorMarshall_;
 };
 
-class ChunkMarshall: public ConstantMarshallImp{
+class EXPORT_DECL ChunkMarshall: public ConstantMarshallImp{
 public:
 	ChunkMarshall(const DataOutputStreamSP& out):ConstantMarshallImp(out){}
 	virtual ~ChunkMarshall(){}
@@ -151,7 +156,7 @@ public:
 	virtual void reset();
 };
 
-class ScalarUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL ScalarUnmarshall: public ConstantUnmarshallImp{
 public:
 	ScalarUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), isCodeObject_(false), functionType_(-1){}
 	virtual ~ScalarUnmarshall(){}
@@ -163,7 +168,7 @@ private:
 	char functionType_;
 };
 
-class VectorUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL VectorUnmarshall: public ConstantUnmarshallImp{
 public:
 	VectorUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), flag_(0), rows_(0), columns_(0), nextStart_(0), unmarshall_(0){}
 	virtual ~VectorUnmarshall(){}
@@ -177,7 +182,7 @@ private:
 	ConstantUnmarshallSP unmarshall_;
 };
 
-class MatrixUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL MatrixUnmarshall: public ConstantUnmarshallImp{
 public:
 	MatrixUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), labelFlag_(-1), rowLabelReceived_(false),
 		columnLabelReceived_(false), inProgress_(false), vectorUnmarshall_(in){}
@@ -194,7 +199,7 @@ private:
 	VectorUnmarshall vectorUnmarshall_;
 };
 
-class TableUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL TableUnmarshall: public ConstantUnmarshallImp{
 public:
 	TableUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), type_(BASICTBL), tableNameReceived_(false), columnNameReceived_(false),
 		nextColumn_(0), inProgress_(false), rows_(0), columns_(0), vectorUnmarshall_(in){}
@@ -215,7 +220,7 @@ private:
 	VectorUnmarshall vectorUnmarshall_;
 };
 
-class SetUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL SetUnmarshall: public ConstantUnmarshallImp{
 public:
 	SetUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), inProgress_(false), vectorUnmarshall_(in) {}
 	virtual ~SetUnmarshall(){}
@@ -226,7 +231,7 @@ private:
 	VectorUnmarshall vectorUnmarshall_;
 };
 
-class DictionaryUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL DictionaryUnmarshall: public ConstantUnmarshallImp{
 public:
 	DictionaryUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), keyReceived_(false),
 		inProgress_(false), vectorUnmarshall_(in) {}
@@ -240,7 +245,7 @@ private:
 	VectorUnmarshall vectorUnmarshall_;
 };
 
-class ChunkUnmarshall: public ConstantUnmarshallImp{
+class EXPORT_DECL ChunkUnmarshall: public ConstantUnmarshallImp{
 public:
 	ChunkUnmarshall(const DataInputStreamSP& in):ConstantUnmarshallImp(in), size_(-1){}
 	virtual ~ChunkUnmarshall(){}
@@ -253,7 +258,7 @@ private:
 	short size_;
 };
 
-class ConstantMarshallFactory{
+class EXPORT_DECL ConstantMarshallFactory{
 public:
 	ConstantMarshallFactory(const DataOutputStreamSP& out);
 	~ConstantMarshallFactory();
@@ -264,7 +269,7 @@ private:
 	ConstantMarshall* arrMarshall[9];
 };
 
-class ConstantUnmarshallFactory{
+class EXPORT_DECL ConstantUnmarshallFactory{
 public:
 	ConstantUnmarshallFactory(const DataInputStreamSP& in);
 	~ConstantUnmarshallFactory();
