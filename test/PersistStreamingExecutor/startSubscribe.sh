630fcdf0bcacd3c13623502c68ec718c54833a3e
Jinzhi Chen <jinzhi.chen@dolphindb.com>
2023-11-14 11:05:42 +0800

    update

--- test/PersistStreamingExecutor/startSubscribe.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
-if [ $1 == "normal" ];then
-    nohup ./demo7 > output7.log 2>&1 &
-elif [ $1 == "HA" ];then
-    nohup ./demo7_HA > output7_HA.log 2>&1 &
-else
-    nohup ./demo7_d > output7_d.log 2>&1 &
-fi
